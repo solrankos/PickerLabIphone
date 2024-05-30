1671c3223b2bce3934bcefe54ba34327701750b6
Oscar Karlsson <karlsson.oscar@gmail.com>
2024-05-30 14:47:22 +0200

    Initial Commit

---
+++ PickerLabIphone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PickerLabIphone
+//
+//  Created by Oscar Karlsson on 2024-05-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PickerLabIphone/PickerLabIphoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerLabIphoneApp.swift
+//  PickerLabIphone
+//
+//  Created by Oscar Karlsson on 2024-05-30.
+//
+
+import SwiftUI
+
+@main
+struct PickerLabIphoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
